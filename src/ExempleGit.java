164017a7815f1cfc0643703914be3cc41de1419d
Loucel1978 <loucelpinos@gmail.com>
2020-02-07 13:01:36 +0100

    actualitzat

--- src/ExempleGit.java
+++ src/ExempleGit.java
@@ -3,7 +3,7 @@ public class ExempleGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola que tal? gent..");
+		System.out.println("Hola que tal? gent.");
 
 	}
 
